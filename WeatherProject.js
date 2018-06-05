1a29917fa8fb40d8fe4aeab282f2f1a3505eef72
pkprosol <peter.prosol@gmail.com>
2018-06-05 15:57:28 -0400

    first commit

---
+++ WeatherProject.js
@@ -0,0 +1,110 @@
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ImageBackground,
+} from 'react-native'
+import Forecast from './Forecast'
+import OpenWeatherMap from './open_weather_map'
+
+class WeatherProject extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      zip: "",
+      forecast: null,
+    }
+  }
+
+  _handleTextChange = event => {
+    let zip = event.nativeEvent.text
+    OpenWeatherMap.fetchForecast(zip).then(forecast => {
+      this.setState({ forecast: forecast })
+    })
+  }
+
+  render() {
+    let content = null
+    if (this.state.forecast !== null) {
+      content = (
+        <Forecast
+          main={this.state.forecast.main}
+          description={this.state.forecast.description}
+          temp={this.state.forecast.temp} />
+      )
+    }
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require('./flowers.png')}
+          resizeMode='cover'
+          style={styles.backdrop}>
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>
+                Current weather for 
+              </Text>
+              <View style={styles.zipContainer}>
+                <TextInput
+                  style={[styles.zipCode, styles.mainText]}
+                  onSubmitEditing={this._handleTextChange}
+                  underlineColorAndroid='transparent' />
+              </View>
+            </View>  
+            {content}
+          </View>
+        </ImageBackground>
+      </View>
+    )
+  }
+}
+
+const baseFontSize = 16
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 30,
+    width: '100%'
+  },
+  backdrop: {
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%'
+  },
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: '#000000',
+    opacity: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'flex-start',
+    padding: 30,
+  },
+  zipContainer: {
+    borderBottomColor: '#DDDDDD',
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  },
+  zipCode: {
+    flex: 1,
+    flexBasis: 1,
+    width: 50,
+    height: baseFontSize,
+    textAlign: 'center'
+  },
+  mainText: {
+    fontSize: baseFontSize,
+    color: '#FFFFFF'
+  }
+})
+
+export default WeatherProject
